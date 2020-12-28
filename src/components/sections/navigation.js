2188b6daab8a7a9e4c33cb8e7ee7e24c9b3d775a
5adiyah <sadiali@pdx.edu>
2020-12-27 22:58:32 -0800

    Add navigation setup

--- src/components/sections/navigation.js
+++ src/components/sections/navigation.js
@@ -5,25 +5,27 @@ import { Link } from "gatsby"
 
 export default function Navigation() {
     return (
-      <header>
-          <div className="menu-bar bg-offwhite">
-              <img src={Menubtn} alt="Click to open the menu"/>
-          </div>
+      <div className="navigation">
+          <div className="container">
+              <div className="menu-bar bg-offwhite">
+                  <img src={Menubtn} alt="Click to open the menu"/>
+              </div>
 
-          <div className="menu-wrapper bg-yellow">
-              <div className="menu-close">
-                  <img src={Menuclose} alt="Click to close the menu"/>
-              </div>
-              <div className="menu">
-                  <ul className="menu-links">
-                      <li><Link to="/">Home</Link></li>
-                      <li><Link to="/about">About</Link></li>
-                      <li><Link to="/testimonials">Testimonials</Link></li>
-                      <li><Link to="/blog">Blog</Link></li>
-                      <li><Link to="/contact">Contact</Link></li>
-                  </ul>
+              <div className="menu-wrapper bg-yellow">
+                  <div className="menu-close">
+                      <img src={Menuclose} alt="Click to close the menu"/>
+                  </div>
+                  <div className="menu">
+                      <ul className="menu-links">
+                          <li><Link to="/">Home</Link></li>
+                          <li><Link to="/about">About</Link></li>
+                          <li><Link to="/testimonials">Testimonials</Link></li>
+                          <li><Link to="/blog">Blog</Link></li>
+                          <li><Link to="/contact">Contact</Link></li>
+                      </ul>
+                  </div>
               </div>
           </div>
-      </header>
+      </div>
     );
 }
