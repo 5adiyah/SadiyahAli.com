72a9f7ff5ad2c124142c26617b8dc6377e29f6da
5adiyah <sadiali@pdx.edu>
2020-12-28 19:20:50 -0800

    Add the rest of the styliing for the hero sectoin

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -5,14 +5,16 @@ import { Link } from "gatsby"
 export default function Navigation() {
     return (
         <header>
-            <div className="hero home bg-offwhite triangle">
-                <div className="img-wrapper">
-                    <img src={Sadiyah} alt="Profile Headshot image of Author"/>
-                </div>
-                <div className="text-wrapper">
-                    <p className="hero-subtitle">Software Engineer</p>
-                    <h1>Sadiyah Ali</h1>
-                    <button className="btn bg-blue white"><Link to="/contact">Contact</Link></button>
+            <div className="hero home bg-lightoffwhite triangle container">
+                <div className="content-side-padding">
+                    <div className="img-wrapper">
+                        <img src={Sadiyah} alt="Profile Headshot image of Author"/>
+                    </div>
+                    <div className="text-wrapper">
+                        <p className="hero-subtitle">Software Engineer</p>
+                        <h1>Sadiyah Ali</h1>
+                        <button className="btn bg-blue white"><Link to="/contact">Contact</Link></button>
+                    </div>
                 </div>
             </div>
         </header>
