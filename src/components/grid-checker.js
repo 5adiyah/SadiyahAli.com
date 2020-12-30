e0f00631005a76b802b6a108deb5f52b1c2e7ac7
5adiyah <sadiali@pdx.edu>
2020-12-29 18:11:29 -0800

    Fix grid

--- src/components/grid-checker.js
+++ src/components/grid-checker.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Grid() {
     return (
         <div id="grid-checker" className="grid">
-            <p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+            <p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p>
         </div>
     )
 }
