b498972a95c38d0ad9b9abf1cadebf1c85d0ba72
5adiyah <sadiali@pdx.edu>
2020-12-29 22:30:29 -0800

    Add blog query - temp

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import Layout from "../components/layout";
+import {graphql} from "gatsby";
 
 export default function Blog() {
     return (
-        <>
+        <Layout>
             <p>Hey! I'm the blog page</p>
-        </>
+        </Layout>
     );
 }
+
+export const query = graphql`
+    query PostQuery {
+        allSanityPost {
+          nodes {
+            id
+            name
+            date
+            content {
+              _key
+              _type
+              style
+              list
+              _rawChildren
+            }
+            hero {
+              asset {
+                fluid(maxWidth: 400) {
+                    ...GatsbySanityImageFluid
+                }
+              }
+            }
+          }
+        }
+    }
+`;
