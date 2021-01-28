9038443c909f6967611b8d669407880eee64cedf
5adiyah <sadiali@pdx.edu>
2021-01-28 02:27:27 -0800

    Add intro section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,28 +5,45 @@ import SEO from "../components/seo"
 import {graphql, StaticQuery} from "gatsby";
 import ReactMarkdown from "react-markdown";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-          <div className="intro grid yellow-before">
-              <div className="full-width">
-                  <h2 className="yellow-line">introo title</h2>
-                  {/*<ReactMarkdown source={document.intro} />*/}
-              </div>
-          </div>
+export default function IndexPage({ data }) {
+    const intro = data.intros.nodes;
+    console.clear();
+    console.log(intro);
+    return(
+        <Layout>
+            <SEO title="Home"/>
+            <div className="intro grid yellow-before">
+                {intro.map((homepg) => (
+                    <div className="full-width" key={homepg.name}>
+                        <h1>{homepg.pagetitle}</h1>
+                        <p>{homepg.content}</p>
+                    </div>
+                ))}
+            </div>
 
-          <div className="experience grid">
-              <div className="full-width">
-                  <h2 className="yellow-line"> experience title </h2>
-              </div>
-          </div>
+            <div className="experience grid">
+                <div className="full-width">
+                    <h2 className="yellow-line"> experience title </h2>
+                </div>
+            </div>
 
-          <div className="skills grid">
-              <div className="full-width">
-                  <h2 className="yellow-line"> skills title</h2>
-              </div>
-          </div>
-  </Layout>
-)
+            <div className="skills grid">
+                <div className="full-width">
+                    <h2 className="yellow-line"> skills title</h2>
+                </div>
+            </div>
+        </Layout>
+    );
+};
 
-export default IndexPage
+export const query = graphql`
+    query IntroQuery {
+      intros: allSanityIntro {
+        nodes {
+            pagetitle
+            content
+            name
+        }
+      }
+    }
+`;
