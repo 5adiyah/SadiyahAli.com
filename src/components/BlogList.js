9acce8bc1ddb3af156db95b108bf69f909659fa4
5adiyah <sadiali@pdx.edu>
2021-01-27 22:24:54 -0800

    Create single post component and pass in the data

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+function BlogItem({ post }) {
+    return (
+        <div className="blog-item">
+            <p>{post.name}</p>
+        </div>
+    )
+}
+
 export default function BlogList( { posts } ) {
     return (
-        <p>List of Posts: {posts.length}</p>
+        <div className="blog-list-wrapper">
+            {posts.map((post) => (
+                <BlogItem key={post.id} post={post} />
+            ))}
+        </div>
+
     );
 }
